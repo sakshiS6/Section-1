0f789b642e06716e03bef837fff2606e0397c763
sakshiS6 <sakshimishra2004545@gmail.com>
2024-08-28 11:13:53 +0530

    conditional operator in javascript

---
+++ JAVASCRIPT/Datatype.js
@@ -0,0 +1,12 @@
+let a=34
+let b=12
+sum=a+b
+console.log(sum)
+console.log(typeof sum)
+
+//area of circle
+
+let r=2
+let pi=3.14
+area=pi*r*r
+console.log(area)
---
+++ JAVASCRIPT/conditions.js
@@ -0,0 +1,46 @@
+let age=19;
+if(age>=18){
+    console.log("You are eligible to vote")
+}
+else{
+    console.log("You can not vote")
+}
+
+
+//----------------------------------------------
+
+let m=99;
+if (m>=90){
+    console.log("A+")
+}else if(m>=80){
+    console.log("A")
+}else if(m>=70){
+    console.log("B")
+}else if(m>=60){
+    console.log("C")
+}else{
+    console.log("Fail")
+}
+
+
+
+//---------------------------------
+
+let char="R";
+if(char=="A" || char=="E" || char=="I" || char=="O" || char=="U")
+{
+    console.log("Vowel")
+}
+else{
+    console.log("Consonant")
+}
+
+
+//-----------------------------------
+ 
+let n=8;
+if(n%2==0){
+    console.log("even")
+}else{
+    console.log("Odd")
+}
