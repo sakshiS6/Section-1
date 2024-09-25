8c4601c2f07825f93b42100637b13c8bfb2d4e02
sakshiS6 <sakshimishra2004545@gmail.com>
2024-09-25 06:47:04 +0530

    Basic Document Object Model

---
+++ JAVASCRIPT/DOM.js
@@ -0,0 +1,11 @@
+//The main adv of using DOM is to do dynamic changes in our web page.
+
+console.log("hello1");
+// alert('hello2')
+// window.console.log("HEllO3")
+console.dir(document.body);
+console.dir(document.body.childNodes);
+console.log(document.body);
+document.body.childNodes[1].innerText = "cbjdvcsks"; //changing heading , if written the console window of browser(inspect>console) then it is called dynamic changes
+
+//DOM Manipulation
